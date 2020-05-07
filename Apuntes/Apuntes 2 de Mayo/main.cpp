a6c37b5668840d7e8e9f8f7ab728b2ce4693d5ad
Risuxc <64237804+Risuxc@users.noreply.github.com>
2020-05-07 02:51:18 -0400

    Implementacion conexion a BDD DBeaver
    Implementacion conexion a BDD DBeaver hecha por el profesor.

---
+++ Apuntes/Apuntes 2 de Mayo/main.cpp
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: risu
+ *
+ * Created on 6 de mayo de 2020, 18:19
+ */
+
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <random>
+#include <cstdlib>
+#include "db.h"
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
