f09993b6b9797d1258cd5e86c7b2f05447727d5b
luisfelipej <ljana@saltala.com>
2020-05-31 18:52:09 -0400

    finalizado script dart

--- Taller 3/main.dart
+++ Taller 3/main.dart
@@ -1,11 +1,14 @@
 import 'dart:io';
 import 'dart:convert';
 import 'dart:async';
+import 'dart:math';
 
-double getAvg(List<String> califications) {
-  var total = 0;
-  califications.forEach((calification) => total += int.parse(calification));
-  return total / califications.length;
+printScores(String scoreName, Score score) {
+  print("\n===${scoreName}===");
+  print("Promedio: ${score.average}");
+  print("Desviación Estandar: ${score.getStDev()} ");
+  print("Moda: ${score.getTrend()}");
+  print("Mediana: ${score.getMed()} ");
 }
 
 manageLine(String line, Map scores) {
@@ -22,15 +25,18 @@ class Score {
   List<double> uniqueNumbers;
   List<int> totalViewed;
   double summedValues;
+  var totalCount;
 
   Score() {
     this.uniqueNumbers = new List();
     this.totalViewed = new List();
     this.summedValues = 0;
+    this.totalCount = 0;
   }
 
   set addItem(double item) {
     summedValues += item;
+    totalCount += 1;
     var index = uniqueNumbers.indexOf(item);
     if (index == -1) {
       //Valor nunca antes visto, se añade a la lista
@@ -41,6 +47,34 @@ class Score {
       totalViewed[index] += 1;
     }
   }
+
+  get average => summedValues / totalCount;
+  double getStDev() {
+    double average = this.average;
+    double variance = 0;
+    uniqueNumbers.forEach(
+        (uniqueNumber) => variance += pow((uniqueNumber - average), 2.0));
+    return sqrt(variance / totalCount);
+  }
+
+  double getTrend() {
+    int bigestIndex = 0;
+    totalViewed.forEach((number) {
+      if (number > bigestIndex) {
+        bigestIndex = totalViewed.indexOf(number);
+      }
+    });
+    return uniqueNumbers[bigestIndex];
+  }
+
+  double getMed() {
+    uniqueNumbers.sort();
+    int halfIndex = uniqueNumbers.length ~/ 2;
+    if (uniqueNumbers.length % 2 == 0) {
+      return (uniqueNumbers[halfIndex] + uniqueNumbers[halfIndex - 1]) / 2;
+    }
+    return uniqueNumbers[halfIndex];
+  }
 }
 
 Future<void> main(List<String> argv) async {
@@ -48,6 +82,14 @@ Future<void> main(List<String> argv) async {
     if (argv.isEmpty) {
       throw 'Indique la ruta del archivo';
     }
+    var scoreNames = [
+      "NEM",
+      "RANKING",
+      "MATEMATICAS",
+      "LENGUAJE",
+      "CIENCIAS",
+      "HISTORIA",
+    ];
     var scores = {
       // NEM
       0: Score(),
@@ -68,6 +110,8 @@ Future<void> main(List<String> argv) async {
         .map(utf8.decode)
         .transform(new LineSplitter())
         .forEach((l) => manageLine(l, scores))
+        .then((l) => scores
+            .forEach((index, score) => printScores(scoreNames[index], score)))
         .catchError(() => throw 'No se pudo abrir el archivo');
   } catch (e) {
     print(e);
